6f60df4c6d3e757052dc086b8c606c48d72afffe
Nikhil Sarwara <117506262+Nikhil-Sarwara@users.noreply.github.com>
2025-02-17 19:27:48 +1100

    Add authentication and authorization features
    Implemented user registration and login in AuthenticationController. Added secured endpoint in TestController. Defined data models for auth in AuthModels. Updated Swagger for JWT Bearer authentication and modified appsettings for testing configurations.

--- SmartInventory.API/Program.cs
+++ SmartInventory.API/Program.cs
@@ -8,6 +8,7 @@ using System;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using SmartInventory.API.Models;
+using Microsoft.OpenApi.Models;
 
 namespace SmartInventoryAPI;
 
@@ -23,12 +24,36 @@ public class Program
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer"
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+        });
 
         // Configure PostgreSQL
         builder.Services.AddDbContext<AppDbContext>(options =>
             options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+        
         // Add Identity
         builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
             .AddEntityFrameworkStores<AppDbContext>()
