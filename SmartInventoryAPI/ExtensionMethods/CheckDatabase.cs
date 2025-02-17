44f0286a9114bbb4c35f74eaf30e4aa7a607f619
Nikhil Sarwara <117506262+Nikhil-Sarwara@users.noreply.github.com>
2025-02-17 13:05:33 +1100

    Add database migration support in CheckDatabase.cs
    Introduced a new using directive for Microsoft.EntityFrameworkCore
    and added a static class DbInitializer with a method
    CheckAndMigrateDatabase to ensure the database is created
    and apply any pending migrations.

--- SmartInventoryAPI/ExtensionMethods/CheckDatabase.cs
+++ SmartInventoryAPI/ExtensionMethods/CheckDatabase.cs
@@ -1,4 +1,5 @@
-﻿using SmartInventoryAPI.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SmartInventoryAPI.Data;
 
 namespace SmartInventoryAPI.ExtensionMethods
 {
@@ -7,6 +8,17 @@ namespace SmartInventoryAPI.ExtensionMethods
         public static void Check(AppDbContext context)
         {
             context.Database.EnsureCreated();
+        }
+    }
+    public static class DbInitializer
+    {
+        public static void CheckAndMigrateDatabase(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            // Ensure the database is created and apply any pending migrations
+            dbContext.Database.Migrate();
         }
     }
 }
